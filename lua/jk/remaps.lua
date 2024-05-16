815215f08ceadb0f5cc4ba69757066e4de3c7722
josip <josipkeresman@gmail.com>
2024-05-16 13:59:25 +0200

    Remove sfml specific remap

--- lua/jk/remaps.lua
+++ lua/jk/remaps.lua
@@ -4,5 +4,3 @@ vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>b", vim.cmd.bprevious)
 vim.keymap.set("n", "<leader>v", vim.cmd.vs)
 vim.keymap.set("n", "<leader>s", vim.cmd.so)
-
-vim.keymap.set("n", "<leader>sf", "<cmd>!g++ -c main.cpp && g++ main.o -o sfml-app -lsfml-graphics -lsfml-window -lsfml-system<CR>");
