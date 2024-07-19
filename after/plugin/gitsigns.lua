6f3d1a1a57ffa5cd83edba6ad8256605f8b7c299
josip <josipkeresman@gmail.com>
2024-07-19 20:10:38 +0200

    Remove trailing whitespace

--- after/plugin/gitsigns.lua
+++ after/plugin/gitsigns.lua
@@ -7,10 +7,10 @@ require('gitsigns').setup {
         changedelete = { text = '~' },
         untracked    = { text = '┆' },
     },
-    signcolumn = true,  
-    numhl      = false, 
-    linehl     = false, 
-    word_diff  = false, 
+    signcolumn = true,
+    numhl      = false,
+    linehl     = false,
+    word_diff  = false,
     watch_gitdir = {
         follow_files = true
     },
@@ -19,7 +19,7 @@ require('gitsigns').setup {
     current_line_blame = false,
     current_line_blame_opts = {
         virt_text = true,
-        virt_text_pos = 'eol', 
+        virt_text_pos = 'eol',
         delay = 1000,
         ignore_whitespace = false,
         virt_text_priority = 100,
@@ -27,8 +27,8 @@ require('gitsigns').setup {
     current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
     sign_priority = 6,
     update_debounce = 100,
-    status_formatter = nil, 
-    max_file_length = 40000, 
+    status_formatter = nil,
+    max_file_length = 40000,
     preview_config = {
         border = 'single',
         style = 'minimal',
