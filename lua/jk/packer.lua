ff7579146333e1358850498e8db532a38d24f0bc
josip <josipkeresman@gmail.com>
2024-09-28 11:37:01 +0200

    Fix formatting in packer.lua

--- lua/jk/packer.lua
+++ lua/jk/packer.lua
@@ -54,14 +54,13 @@ return require("packer").startup(function(use)
 
     use("ThePrimeagen/harpoon")
 
-    use {
+    use({
         "rcarriga/nvim-notify",
         config = function()
             require("notify").setup({
                 stages = "fade_in_slide_out",
             })
             vim.notify = require("notify")
-        end
-    }
-
+        end,
+    })
 end)
