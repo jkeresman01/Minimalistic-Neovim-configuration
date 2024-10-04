472cf841369bbfe0d5ab605a00bff5e0f4a6c7d1
josip <josipkeresman@gmail.com>
2024-10-04 19:58:28 +0200

    Add additional LSP mappings

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -5,11 +5,20 @@ lsp_zero.on_attach(function(bufnr)
         buffer = bufnr,
         preserve_mappings = false,
     })
+
+    local opts = { noremap=true, silent=true, buffer=bufnr }
+
+    vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
+    vim.api.nvim_buf_set_keymap(bufnr, "n", "K", "<Cmd>lua vim.lsp.buf.hover()<CR>", opts)
+    vim.api.nvim_buf_set_keymap(bufnr, "n", "gr", "<Cmd>lua vim.lsp.buf.references()<CR>", opts)
+    vim.api.nvim_buf_set_keymap(bufnr, "n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+    vim.api.nvim_buf_set_keymap(bufnr, "n", "gi", "<Cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+    vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>rn", "<Cmd>lua vim.lsp.buf.rename()<CR>", opts)
 end)
 
 require("mason").setup({})
 require("mason-lspconfig").setup({
-    ensure_installed = { "tsserver", "rust_analyzer", "clangd", "gopls" },
+    ensure_installed = {"rust_analyzer", "clangd", "gopls" , "omnisharp" },
     handlers = { lsp_zero.default_setup },
 })
 
