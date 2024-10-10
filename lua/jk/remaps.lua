0f86a82f0b9b70d7f3bf95ee2b32de7cda11178f
josip <josipkeresman@gmail.com>
2024-10-10 23:26:18 +0200

    Change remap for explore view

--- lua/jk/remaps.lua
+++ lua/jk/remaps.lua
@@ -1,6 +1,6 @@
 vim.g.mapleader = ","
 
-vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>e", vim.cmd.Explore)
 vim.keymap.set("n", "<leader>b", vim.cmd.bprevious)
 vim.keymap.set("n", "<leader>v", vim.cmd.vs)
 vim.keymap.set("n", "<leader>s", vim.cmd.so)
