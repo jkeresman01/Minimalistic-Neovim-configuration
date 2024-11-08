6893f29864a04ecb9456d4170b158d87a275d166
josip <josipkeresman@gmail.com>
2024-11-08 15:26:02 +0100

    Add snippet support in lsp config

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -19,20 +19,61 @@ end)
 
 require("mason").setup({})
 require("mason-lspconfig").setup({
-    ensure_installed = {"rust_analyzer", "clangd", "gopls" , "omnisharp" },
+    ensure_installed = {"rust_analyzer", "clangd", "gopls", "omnisharp"},
     handlers = { lsp_zero.default_setup },
 })
 
 local cmp = require("cmp")
+local lspkind = require("lspkind")
 
 cmp.setup({
-    sources = { { name = "nvim_lsp" } },
-    mapping = cmp.mapping.preset.insert({
-        ["<CR>"] = cmp.mapping.confirm({ select = false }),
-    }),
     snippet = {
         expand = function(args)
             require("luasnip").lsp_expand(args.body)
         end,
     },
+
+    sources = {
+        { name = "nvim_lsp" },
+        { name = "luasnip" },
+        { name = "buffer" },
+        { name = "path" }
+    },
+
+    mapping = cmp.mapping.preset.insert({
+        ["<CR>"] = cmp.mapping.confirm({ select = false }),
+        ["<Tab>"] = cmp.mapping.select_next_item(),
+        ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+    }),
+
+    formatting = {
+        format = lspkind.cmp_format({
+            mode = "symbol_text",
+            maxwidth = 50,
+            ellipsis_char = "...",
+        }),
+    },
 })
+
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+require("lspconfig").rust_analyzer.setup({
+    capabilities = capabilities,
+    on_attach = lsp_zero.on_attach,
+})
+
+require("lspconfig").clangd.setup({
+    capabilities = capabilities,
+    on_attach = lsp_zero.on_attach,
+})
+
+require("lspconfig").gopls.setup({
+    capabilities = capabilities,
+    on_attach = lsp_zero.on_attach,
+})
+
+require("lspconfig").omnisharp.setup({
+    capabilities = capabilities,
+    on_attach = lsp_zero.on_attach,
+})
+
